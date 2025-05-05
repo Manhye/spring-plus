f2a6872cc9d82a88be5b447604051684ea1ce8ec
kibeo <kibeom0806@gmail.com>
2025-05-05 23:18:07 +0900

    aop 수정

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -7,6 +7,9 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -21,7 +24,15 @@ public class AdminAccessLoggingAspect {
 
     @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
     public void logBeforeChangeUserRole(JoinPoint joinPoint) {
-        String userId = String.valueOf(request.getAttribute("userId"));
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String userId = "NONE";
+
+        if (authentication != null  && authentication.getPrincipal() instanceof AuthUser authUser) {
+            userId = String.valueOf(authUser.getId());
+        }
+
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
 
