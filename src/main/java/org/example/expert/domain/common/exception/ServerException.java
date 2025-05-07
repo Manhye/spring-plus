8b72495329ec14de42396754474befcc1914192d
kibeo <kibeom0806@gmail.com>
2025-05-07 13:47:11 +0900

    Kotlin/domain/comment

--- src/main/java/org/example/expert/domain/common/exception/ServerException.java
+++ src/main/java/org/example/expert/domain/common/exception/ServerException.java
@@ -1,8 +1,8 @@
-package org.example.expert.domain.common.exception;
-
-public class ServerException extends RuntimeException {
-
-    public ServerException(String message) {
-        super(message);
-    }
-}
+// package org.example.expert.domain.common.exception;
+//
+// public class ServerException extends RuntimeException {
+//
+//     public ServerException(String message) {
+//         super(message);
+//     }
+// }
