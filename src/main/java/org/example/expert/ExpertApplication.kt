6bc72c8851d258087cae89d2c2c731f6fc46ee67
kibeo <kibeom0806@gmail.com>
2025-05-12 12:40:09 +0900

    main method

---
+++ src/main/java/org/example/expert/ExpertApplication.kt
@@ -0,0 +1,16 @@
+package org.example.expert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+
+@SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+open class ExpertApplication{}
+
+fun main(args: Array<String>) {
+    runApplication<ExpertApplication>(*args)
+}
+
+
